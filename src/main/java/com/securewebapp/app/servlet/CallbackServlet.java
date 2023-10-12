4c5fc23e9dee5ba443d616d1ab5f138d82579f6c
santhossunthar <santhoshsunthar@gmail.com>
2023-10-12 21:20:58 +0530

    Added CallbackServlet changes

--- src/main/java/com/securewebapp/app/servlet/CallbackServlet.java
+++ src/main/java/com/securewebapp/app/servlet/CallbackServlet.java
@@ -1,58 +1,60 @@
 package com.securewebapp.app.servlet;
 
+import com.auth0.AuthenticationController;
+import com.auth0.IdentityVerificationException;
+import com.auth0.SessionUtils;
+import com.auth0.Tokens;
+import com.securewebapp.app.auth.*;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import java.io.IOException;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.securewebapp.app.auth.AuthUser;
-import com.securewebapp.app.auth.TokenHandler;
-
 public class CallbackServlet extends HttpServlet {
-    private Algorithm algorithm = Algorithm.HMAC256("BcmIMR3Un8s1Q_dwwIU3S-n4RZx2X4aNkAJxCP_8eDp0wgYKjlhr3m-dynHbT2-C");
-    private JWTVerifier verifier = JWT.require(algorithm).build();
-    private String[] tokens = null;
+    private String redirectOnSuccess;
+    private String redirectOnFail;
+    private AuthenticationController authenticationController;
+    private AuthConfig authConfig;
 
-    protected void doGet(
-            HttpServletRequest request,
-            HttpServletResponse response
-    ) throws ServletException, IOException {
-        String code = request.getParameter("code");
+    @Override
+    public void init() throws ServletException {
+        redirectOnSuccess = "/dashboard";
+        redirectOnFail = "/login";
 
-        // Getting ID Token
-        TokenHandler tokenHandler = new TokenHandler();
-        tokens = tokenHandler.exchangeCodeForIdToken(code);
-        System.out.println(tokens[1]);
+        AuthConfig configs = new AuthConfig();
+        AuthenticationProvider authenticationProvider = new AuthenticationProvider();
+        authenticationController = authenticationProvider.authenticationController(configs);
+    }
 
-        AuthUser authUser = new AuthUser();
-        authUser.getInfo(tokens[1]);
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        handle(req, res);
+    }
 
-        // Verify ID Token
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        handle(req, res);
+    }
+
+    private void handle(HttpServletRequest req, HttpServletResponse res) throws IOException {
         try {
-            DecodedJWT jwt = verifier.verify(tokens[0]);
+            Tokens tokens = authenticationController.handle(req, res);
+            SessionUtils.set(req, "accessToken", tokens.getAccessToken());
+            SessionUtils.set(req, "idToken", tokens.getIdToken());
 
-            // Check if the JWT contains required claims and is not expired
-            if (jwt.getClaims().containsKey("sub")) {
-                String userId = jwt.getClaim("sid").asString();
+            HttpSession session = req.getSession(true);
+            session.setAttribute("userId", session.getId());
 
-                // Create a session and store user information
-                HttpSession session = request.getSession(true);
-                session.setAttribute("userId", userId);
+            Cookie accessTokenCookie = new Cookie("token", tokens.getIdToken());
+            accessTokenCookie.setPath("/");
+            accessTokenCookie.setHttpOnly(true);
+            accessTokenCookie.setMaxAge(3600);
+            res.addCookie(accessTokenCookie);
 
-                Cookie accessTokenCookie = new Cookie("accessToken", tokens[1]);
-                accessTokenCookie.setPath("/");
-                accessTokenCookie.setMaxAge(3600);
-                response.addCookie(accessTokenCookie);
-                response.sendRedirect("/dashboard");
-            } else {
-                response.sendRedirect("index.jsp?error=AuthenticationFailed");
-            }
-        } catch (JWTVerificationException e) {
-            response.sendRedirect("index.jsp?error=AuthenticationError");
+            res.sendRedirect(redirectOnSuccess);
+        } catch (IdentityVerificationException e) {
+            res.sendRedirect(redirectOnFail);
         }
     }
+
 }
