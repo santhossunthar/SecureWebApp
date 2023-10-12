4c379f3eafa513da08a3de94460d42b865c0a659
santhossunthar <santhoshsunthar@gmail.com>
2023-10-12 21:21:33 +0530

    Added DashboardServlet changes

--- src/main/java/com/securewebapp/app/servlet/DashboardServlet.java
+++ src/main/java/com/securewebapp/app/servlet/DashboardServlet.java
@@ -1,8 +1,11 @@
 package com.securewebapp.app.servlet;
 
-import com.securewebapp.app.db.DBConnection;
+import com.securewebapp.app.auth.JwtCredential;
+import com.securewebapp.app.auth.JwtPrincipal;
+import com.securewebapp.app.repository.ReservRepository;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,12 +17,23 @@ public class DashboardServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        String user = (String) req.getSession(true)
-                .getAttribute("userId");
-        req.setAttribute("user", user);
+        String idToken;
+        List<HashMap<String, Object>> reservationsDetails = null;
+        Cookie[] cookies = req.getCookies();
 
-        DBConnection dbConnection = new DBConnection();
-        List<HashMap<String, Object>> reservationsDetails = dbConnection.getReservationsDetails();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("token".equals(cookie.getName())) {
+                    idToken = cookie.getValue();
+                    JwtCredential jwtCredential = new JwtCredential(idToken);
+                    JwtPrincipal jwtPrincipal = jwtCredential.getAuth0JwtPrincipal();
+                    ReservRepository reservRepository = new ReservRepository();
+                    reservationsDetails = reservRepository
+                            .getReservationsDetails(jwtPrincipal.getName());
+                    break;
+                }
+            }
+        }
 
         req.setAttribute("reservationsDetails", reservationsDetails);
         req.getRequestDispatcher("/WEB-INF/jsp/dashboard.jsp")
