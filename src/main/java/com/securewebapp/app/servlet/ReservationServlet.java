fdc18625c7bfd2e6acee1cdd70bf73de5c87aacb
santhossunthar <santhoshsunthar@gmail.com>
2023-11-17 02:02:51 +0530

    Added ReservationServlet changes

--- src/main/java/com/securewebapp/app/servlet/ReservationServlet.java
+++ src/main/java/com/securewebapp/app/servlet/ReservationServlet.java
@@ -40,6 +40,8 @@ public class ReservationServlet extends HttpServlet {
                 HttpSession session = req.getSession(false);
 
                 if (session != null) {
+                    String userId = (String) session.getAttribute("userId");
+
                     String reservationDate = req.getParameter("date");
                     String reservationTime = req.getParameter("time");
                     String reservationLocation = req.getParameter("location");
@@ -47,64 +49,45 @@ public class ReservationServlet extends HttpServlet {
                     String reservationMileage = req.getParameter("mileage");
                     String reservationMessage = req.getParameter("message");
 
-                    if(reservationDate == null
-                            || reservationTime == null
-                            || reservationLocation == null
-                            || reservationVehicleNo == null
-                            || reservationMileage == null
-                            || reservationMessage == null
+                    if(reservationDate != null
+                            && reservationTime != null
+                            && reservationLocation != null
+                            && reservationVehicleNo != null
+                            && reservationMileage != null
+                            && reservationMessage != null
                     ) {
-                        req.setAttribute("msg", "failed");
-                        req.getRequestDispatcher("/WEB-INF/jsp/reservation_post_action.jsp")
-                                .forward(req, resp);
-                        return;
-                    }
+                        HashMap<String, String> postValidatedData = new HashMap<>();
 
-                    HashMap<String, String> postValidatedData = new HashMap<>();
-                    if (InputValidator.isValidDate(reservationDate)
-                            && InputValidator.isNumeric(reservationTime)
-                            && InputValidator.isAlphanumeric(reservationLocation)
-                            && InputValidator.isAlphanumeric(reservationVehicleNo)
-                            && InputValidator.isNumeric(reservationMileage)
-                            && InputValidator.isAlphanumeric(reservationMessage)
-                    ) {
-                        postValidatedData.put("reservationDate", reservationDate);
-                        postValidatedData.put("reservationTime", reservationTime);
-                        postValidatedData.put("reservationLocation", reservationLocation);
-                        postValidatedData.put("reservationVehicleNo", reservationVehicleNo);
-                        postValidatedData.put("reservationMileage", reservationMileage);
-                        postValidatedData.put("reservationMessage", reservationMessage);
+                        if (InputValidator.isValidDate(reservationDate)
+                                && InputValidator.isNumeric(reservationTime)
+                                && InputValidator.isAlphanumeric(reservationLocation)
+                                && InputValidator.isAlphanumeric(reservationVehicleNo)
+                                && InputValidator.isNumeric(reservationMileage)
+                                && InputValidator.isAlphanumeric(reservationMessage)
+                        ) {
+                            postValidatedData.put("reservationDate", reservationDate);
+                            postValidatedData.put("reservationTime", reservationTime);
+                            postValidatedData.put("reservationLocation", reservationLocation);
+                            postValidatedData.put("reservationVehicleNo", reservationVehicleNo);
+                            postValidatedData.put("reservationMileage", reservationMileage);
+                            postValidatedData.put("reservationMessage", reservationMessage);
+                            postValidatedData.put("userName", userId);
 
-                        String idToken;
-                        Cookie[] cookies = req.getCookies();
-                        if (cookies != null) {
-                            for (Cookie cookie : cookies) {
-                                if ("token".equals(cookie.getName())) {
-                                    idToken = cookie.getValue();
-                                    JwtCredential jwtCredential = new JwtCredential(idToken);
-                                    JwtPrincipal jwtPrincipal = jwtCredential.getAuth0JwtPrincipal();
-                                    postValidatedData.put("userName", jwtPrincipal.getName());
-                                    break;
-                                }
+                            ReservRepository reservRepository = new ReservRepository();
+                            if(reservRepository.addReservationDetails(postValidatedData)){
+                                req.setAttribute("msg", "success");
+                                req.getRequestDispatcher("/WEB-INF/jsp/reservation_post_action.jsp")
+                                        .forward(req, resp);
+                            } else {
+                                req.setAttribute("msg", "failed");
+                                req.getRequestDispatcher("/WEB-INF/jsp/reservation_post_action.jsp")
+                                        .forward(req, resp);
                             }
                         } else {
                             req.setAttribute("msg", "failed");
                             req.getRequestDispatcher("/WEB-INF/jsp/reservation_post_action.jsp")
                                     .forward(req, resp);
-                            return;
                         }
-                    } else {
-                        req.setAttribute("msg", "failed");
-                        req.getRequestDispatcher("/WEB-INF/jsp/reservation_post_action.jsp")
-                                .forward(req, resp);
-                        return;
-                    }
-
-                    ReservRepository reservRepository = new ReservRepository();
-                    if(reservRepository.addReservationDetails(postValidatedData)){
-                        req.setAttribute("msg", "success");
-                        req.getRequestDispatcher("/WEB-INF/jsp/reservation_post_action.jsp")
-                                .forward(req, resp);
                     } else {
                         req.setAttribute("msg", "failed");
                         req.getRequestDispatcher("/WEB-INF/jsp/reservation_post_action.jsp")
