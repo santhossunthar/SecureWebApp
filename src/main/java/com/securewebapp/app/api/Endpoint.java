582410d1570bec1bea34c0d10f2a640d785ee632
santhossunthar <santhoshsunthar@gmail.com>
2023-11-19 14:16:53 +0530

    Added APIs

---
+++ src/main/java/com/securewebapp/app/api/Endpoint.java
@@ -0,0 +1,10 @@
+package com.securewebapp.app.api;
+
+public class Endpoint {
+    public static final String login = "/login";
+    public static final String reservation = "/reservation";
+    public static final String reservationAdd = "/reservation/add";
+    public static final String reservationView = "/reservation/view";
+    public static final String reservationDelete = "/reservation/delete";
+    public static final String userProfile = "/profile";
+}
