f182438d62513f6b033a6548b47a768e2184d52d
santhossunthar <santhoshsunthar@gmail.com>
2023-11-21 12:19:09 +0530

    Added RefererFilter changes

--- src/main/java/com/securewebapp/app/filter/RefererFilter.java
+++ src/main/java/com/securewebapp/app/filter/RefererFilter.java
@@ -2,37 +2,46 @@ package com.securewebapp.app.filter;
 
 import com.securewebapp.app.api.ApplicationURL;
 import com.securewebapp.app.api.Endpoint;
+import com.securewebapp.app.auth.AuthConfig;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class RefererFilter implements Filter {
+    private static final Logger logger = Logger.getLogger(AuthConfig.class.getName());
+
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {}
+    public void init(FilterConfig filterConfig) {}
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-            throws IOException, ServletException {
+            throws IOException {
         try{
             HttpServletRequest request = (HttpServletRequest) req;
             HttpServletResponse response = (HttpServletResponse) res;
             String referer = request.getHeader("Referer");
 
-            if (isValidReferer(referer)) {
-                chain.doFilter(req, res);
+            if (referer != null) {
+                if (isValidReferer(referer)) {
+                    chain.doFilter(req, res);
+                } else {
+                    response.sendError(HttpServletResponse.SC_FORBIDDEN, "Invalid Referer");
+                }
             } else {
-                response.sendError(HttpServletResponse.SC_FORBIDDEN, "Invalid Referer");
+                chain.doFilter(req, res);
             }
         } catch (ServletException | IOException ex){
+            logger.log(Level.SEVERE, "An error occurred: " + ex.getMessage(), ex);
             ((HttpServletResponse) res).sendRedirect(Endpoint.root);
         }
     }
 
     private boolean isValidReferer(String referer) {
-        return referer != null
-                && referer.startsWith(ApplicationURL.getURL());
+        return referer.startsWith(ApplicationURL.getURL());
     }
 
     @Override
